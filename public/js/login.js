12b0f970d2f6df2ccc78ef4c36dc9825dba02f36
Joseph Livengood <joeylivengood@gmail.com>
2016-11-27 08:03:32 +0000

    Completed login page

--- public/js/login.js
+++ public/js/login.js
@@ -3,6 +3,13 @@ var app = angular.module('login', []);
 
 app.controller('LoginCtrl',['$scope', function($scope){
     
-    //
+    $scope.github = function() {
+        /*global $*/
+        $('.login').fadeOut(1000, function() {
+            $('#github-icon').fadeIn(1000, function() {
+                window.location.href = '/auth/github';
+            });
+        });
+    };
     
 }]);
