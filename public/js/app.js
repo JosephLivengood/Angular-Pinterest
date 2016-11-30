1fa8d6ab0c2138579a15cea46f81b4fff8f9846a
Joseph Livengood <joeylivengood@gmail.com>
2016-11-30 00:18:19 +0000

    Revert "Removal of broken modal"
    This reverts commit 4e348b3c38cf1b5ba2b1d8ffb27aeeec5d03c655.

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,40 @@
 var app = angular.module('pinterest', ['ui.bootstrap']);
 
 app.controller('MainCtrl',['$scope', '$modal', '$log', function($scope, $modal, $log){
-    
-    
-    
-    
+    $scope.showForm = function () {
+        $scope.message = "Show Form Button Clicked";
+        console.log($scope.message);
+        var modalInstance = $modal.open({
+            templateUrl: '/newpin',
+            controller: ModalInstanceCtrl,
+            scope: $scope,
+            size: 'lg',
+            resolve: {
+                userForm: function () {
+                    return $scope.userForm;
+                }
+            }
+        });
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
 }]);
+
+var ModalInstanceCtrl = function ($scope, $modalInstance, userForm) {
+    $scope.form = {};
+    $scope.submitForm = function () {
+        if ($scope.form.userForm.$valid) {
+            console.log('user form is in scope');
+            $modalInstance.close('closed');
+        } else {
+            console.log('userform is not in scope');
+        }
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+};
