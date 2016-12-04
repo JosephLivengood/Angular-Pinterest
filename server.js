7f1ecf151a023d4ec7ec8c9e04abff430a632558
Joseph Livengood <joeylivengood@gmail.com>
2016-12-04 07:59:01 +0000

    Fixed env port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /*Server initialisation*/
 var express     =       require('express');
 var app         =       express();
-var PORT        =       process.env.port || 8080;
+var PORT        =       process.env.PORT || 8080;
 var path        =       process.cwd();
 var bodyParser  =       require('body-parser');
 
